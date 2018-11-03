remove step penalty; fix a bug in position calculation; add tensorboad writer

visual_nav/dqn.py
@@ -7,6 +7,7 @@ import logging
 import os
 import argparse
 import shutil
+import pprint
 
 import git
 import gym
@@ -18,12 +19,13 @@ import torch.autograd as autograd
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from tensorboardX import SummaryWriter
 
 from crowd_sim.envs.utils.action import ActionXY
 from crowd_sim.envs.policy.orca import ORCA
 from visual_nav.utils.replay_buffer import ReplayBuffer
 from visual_nav.utils.gym import get_wrapper_by_name
-from visual_nav.utils.schedule import LinearSchedule
+from visual_nav.utils.schedule import LinearSchedule, ConstantSchedule
 from visual_sim.envs.visual_sim import VisualSim
 
 
@@ -44,7 +46,7 @@ Statistic = {
 
 
 class DQN(nn.Module):
-    def __init__(self, in_channels=4, num_actions=18):
+    def __init__(self, in_channels=4, num_actions=18, downsample_image=False):
         """
         Initialize a deep Q-learning network as described in
         https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf
@@ -56,8 +58,12 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc4 = nn.Linear(7 * 7 * 64, 512)
+        if downsample_image:
+            self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+            self.fc4 = nn.Linear(7 * 7 * 64, 512)
+        else:
+            self.conv3 = nn.Conv2d(64, 64, kernel_size=4, stride=2)
+            self.fc4 = nn.Linear(7 * 14 * 64, 512)
         self.fc5 = nn.Linear(520, num_actions)
 
     def forward(self, frames, goals):
@@ -80,31 +86,43 @@ class Trainer(object):
     def __init__(self,
                  env,
                  q_func,
+                 output_dir,
                  replay_buffer_size=1000000,
                  batch_size=128,
                  gamma=0.99,
                  frame_history_len=4,
-                 target_update_freq=10000
+                 target_update_freq=10000,
+                 downsample_image=False
                  ):
         self.env = env
         self.batch_size = batch_size
         self.gamma = gamma
         self.frame_history_len = frame_history_len
         self.target_update_freq = target_update_freq
+        self.output_dir = output_dir
 
         img_h, img_w, img_c = env.observation_space.shape
         input_arg = frame_history_len * img_c
         self.num_actions = env.action_space.n
+        if downsample_image:
+            self.image_size = (84, 84, img_c, downsample_image)
+        else:
+            self.image_size = (img_h, img_w, img_c, downsample_image)
 
-        self.Q = q_func(input_arg, self.num_actions).type(dtype)
-        self.target_Q = q_func(input_arg, self.num_actions).type(dtype)
-        self.replay_buffer = ReplayBuffer(replay_buffer_size, frame_history_len)
+        self.Q = q_func(input_arg, self.num_actions, downsample_image).type(dtype)
+        self.target_Q = q_func(input_arg, self.num_actions, downsample_image).type(dtype)
+        self.replay_buffer = ReplayBuffer(replay_buffer_size, frame_history_len, self.image_size)
 
         self.log_every_n_steps = 10000
         self.num_param_updates = 0
         self.actions = None
 
-    def imitation_learning(self, optimizer_spec, weights_file, demonstrate_steps=50000, update_steps=10000):
+    def imitation_learning(self, optimizer_spec, demonstrate_steps=50000, update_steps=10000):
+        """
+        Imitation learning and reinforcement learning share the same environment, replay buffer and Q function
+
+        """
+        weights_file = os.path.join(self.output_dir, 'il_model.pth')
         # TODO: how to optimize q-value function
         if os.path.exists(weights_file):
             self.Q.load_state_dict(torch.load(weights_file))
@@ -168,7 +186,7 @@ class Trainer(object):
 
     def test(self):
         logging.info('Start testing model')
-        replay_buffer = ReplayBuffer(100000, self.frame_history_len)
+        replay_buffer = ReplayBuffer(100000, self.frame_history_len, self.image_size)
 
         test_case_num = self.env.unwrapped.test_case_num
         success = 0
@@ -181,7 +199,7 @@ class Trainer(object):
             while not done:
                 last_idx = replay_buffer.store_observation(obs)
                 recent_observations = replay_buffer.encode_recent_observation()
-                action = self.select_greedy_action(recent_observations)
+                action = self.act(recent_observations)
                 ob, reward, done, info = self.env.step(action.item())
                 replay_buffer.store_effect(last_idx, action, reward, done)
                 if info == 'Accomplishment':
@@ -198,14 +216,20 @@ class Trainer(object):
         logging.info('Success: {:.2f}, collision: {:.2f}, overtime: {:.2f}, average time: {:.2f}s'.format(
             success / test_case_num, collision / test_case_num, overtime / test_case_num, avg_time))
 
-    def reinforcement_learning(self, optimizer_spec, exploration, weights_file, statistics_file,
-                               stopping_criterion=None, learning_starts=50000, learning_freq=4):
+    def reinforcement_learning(self, optimizer_spec, exploration, stopping_criterion=None, learning_starts=50000,
+                               learning_freq=4):
+        weights_file = os.path.join(self.output_dir, 'rl_model.pth')
+        statistics_file = os.path.join(self.output_dir, 'statistics.pkl')
+        tf_statistics_file = os.path.join(self.output_dir, 'statistics.json')
+
         if os.path.exists(weights_file):
             self.Q.load_state_dict(torch.load(weights_file))
             self.target_Q.load_state_dict(torch.load(weights_file))
             logging.info('Reinforcement learning trained weight loaded')
 
         logging.info('Start reinforcement learning')
+        writer = SummaryWriter()
+        episode_starts = len(get_wrapper_by_name(self.env, "Monitor").get_episode_rewards())
         mean_episode_reward = -float('nan')
         best_mean_episode_reward = -float('inf')
         last_obs = self.env.reset()
@@ -254,7 +278,7 @@ class Trainer(object):
                 self.td_update(optimizer)
 
             # Log progress and keep track of statistics
-            episode_rewards = get_wrapper_by_name(self.env, "Monitor").get_episode_rewards()
+            episode_rewards = get_wrapper_by_name(self.env, "Monitor").get_episode_rewards()[episode_starts:]
             if len(episode_rewards) > 0:
                 mean_episode_reward = np.mean(episode_rewards[-100:])
             if len(episode_rewards) > 100:
@@ -262,6 +286,8 @@ class Trainer(object):
 
             Statistic["mean_episode_rewards"].append(mean_episode_reward)
             Statistic["best_mean_episode_rewards"].append(best_mean_episode_reward)
+            writer.add_scalar('data/mean_episode_rewards', mean_episode_reward, t)
+            writer.add_scalar('data/best_mean_episode_rewards', best_mean_episode_reward, t)
 
             if t % self.log_every_n_steps == 0 and t > learning_starts:
                 logging.info("Timestep %d" % (t,))
@@ -278,6 +304,9 @@ class Trainer(object):
 
                 torch.save(self.Q.state_dict(), weights_file)
 
+        writer.export_scalars_to_json(tf_statistics_file)
+        writer.close()
+
     def select_epsilon_greedy_action(self, model, obs, eps_threshold):
         sample = random.random()
         if sample > eps_threshold:
@@ -288,7 +317,7 @@ class Trainer(object):
         else:
             return torch.IntTensor([random.randrange(self.num_actions)])
 
-    def select_greedy_action(self, obs):
+    def act(self, obs):
         frames = torch.from_numpy(obs[0]).type(dtype).unsqueeze(0) / 255.0
         goals = torch.from_numpy(np.array(obs[1])).type(dtype).unsqueeze(0)
         return self.Q(Variable(frames), Variable(goals)).data.max(1)[1].cpu()
@@ -349,12 +378,23 @@ class Trainer(object):
         if self.num_param_updates % self.target_update_freq == 0:
             self.target_Q.load_state_dict(self.Q.state_dict())
 
+    def mc_update(self):
+        pass
+
 
 def main():
     parser = argparse.ArgumentParser('Parse configuration file')
     parser.add_argument('--output_dir', type=str, default='data/output')
     parser.add_argument('--debug', default=False, action='store_true')
     parser.add_argument('--without_il', default=False, action='store_true')
+    parser.add_argument('--eps_start', type=float, default=1)
+    parser.add_argument('--eps_end', type=float, default=0.1)
+    parser.add_argument('--eps_decay_steps', type=int, default=1000000)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--num_timesteps', type=int, default=2000000)
+    parser.add_argument('--learning_starts', type=int, default=50000)
+    parser.add_argument('--downsample_image', default=False, action='store_true')
     args = parser.parse_args()
 
     # configure paths
@@ -368,9 +408,6 @@ def main():
     if make_new_dir:
         os.makedirs(args.output_dir)
     log_file = os.path.join(args.output_dir, 'output.log')
-    il_weights_file = os.path.join(args.output_dir, 'il_model.pth')
-    rl_weights_file = os.path.join(args.output_dir, 'rl_model.pth')
-    statistics_file = os.path.join(args.output_dir, 'statistics.pkl')
     monitor_dir = os.path.join(args.output_dir, 'monitor-outputs')
 
     # configure logging
@@ -384,22 +421,25 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     logging.info('Using device: %s', device)
 
-    env = VisualSim(step_penalty=-0.01)
-    env = wrappers.Monitor(env, monitor_dir, force=True)
-    num_timesteps = 1000000
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(vars(args))
 
+    # configure environment
+    env = VisualSim()
+    env = wrappers.Monitor(env, monitor_dir, force=True)
     assert type(env.observation_space) == gym.spaces.Box
     assert type(env.action_space) == gym.spaces.Discrete
 
     trainer = Trainer(
         env=env,
         q_func=DQN,
-        # replay_buffer_size=1000000
+        output_dir=args.output_dir,
         replay_buffer_size=100000,
-        batch_size=32,
-        gamma=0.99,
+        batch_size=args.batch_size,
+        gamma=args.gamma,
         frame_history_len=4,
-        target_update_freq=10000
+        target_update_freq=10000,
+        downsample_image=args.downsample_image
     )
 
     # imitation learning
@@ -410,7 +450,6 @@ def main():
     if not args.without_il:
         trainer.imitation_learning(
             optimizer_spec=il_optimizer_spec,
-            weights_file=il_weights_file,
             demonstrate_steps=50000,
             update_steps=100000
         )
@@ -419,21 +458,21 @@ def main():
     def stopping_criterion(env):
         # notice that here t is the number of steps of the wrapped env,
         # which is different from the number of steps in the underlying env
-        return get_wrapper_by_name(env, "Monitor").get_total_steps() >= num_timesteps
+        return get_wrapper_by_name(env, "Monitor").get_total_steps() >= args.num_timesteps
 
     rl_optimizer_spec = OptimizerSpec(
         constructor=optim.RMSprop,
         kwargs=dict(lr=0.00025, alpha=0.95, eps=0.01),
     )
-    exploration_schedule = LinearSchedule(1000000, 0.1)
+    if args.eps_decay_steps == 0:
+        exploration_schedule = ConstantSchedule(0.1)
+    else:
+        exploration_schedule = LinearSchedule(args.eps_decay_steps, args.eps_end, args.eps_start)
     trainer.reinforcement_learning(
         optimizer_spec=rl_optimizer_spec,
         exploration=exploration_schedule,
         stopping_criterion=stopping_criterion,
-        weights_file=rl_weights_file,
-        statistics_file=statistics_file,
-        # learning_starts=50000,
-        learning_starts=50000,
+        learning_starts=args.learning_starts,
         learning_freq=4
     )
 

visual_nav/utils/replay_buffer.py
@@ -2,7 +2,7 @@
     This file is copied/apdated from https://github.com/berkeleydeeprlcourse/homework/tree/master/hw3
 """
 import random
-
+from PIL import Image
 import numpy as np
 
 
@@ -19,7 +19,7 @@ def sample_n_unique(sampling_f, n):
 
 
 class ReplayBuffer(object):
-    def __init__(self, size, frame_history_len):
+    def __init__(self, size, frame_history_len, image_size):
         """This is a memory efficient implementation of the replay buffer.
 
         The specific memory optimizations use here are:
@@ -47,6 +47,7 @@ class ReplayBuffer(object):
         """
         self.size = size
         self.frame_history_len = frame_history_len
+        self.image_size = image_size
 
         self.next_idx = 0
         self.num_in_buffer = 0
@@ -180,6 +181,9 @@ class ReplayBuffer(object):
             Index at which the frame is stored. To be used for `store_effect` later.
         """
         frame = ob.image
+        if frame.shape != self.image_size:
+            assert frame.shape[2] == 1
+            frame = np.expand_dims(Image.fromarray(frame.squeeze()).resize(self.image_size[:2]), axis=2)
         goal = ob.goal
 
         # make sure we are not using low-dimensional observations, such as RAM

visual_sim/envs/visual_sim.py
@@ -42,7 +42,7 @@ class VisualSim(Env):
       SurfaceNormals = 6,
       Infrared = 7
     """
-    def __init__(self, image_type='DepthPerspective', step_penalty=0):
+    def __init__(self, image_type='DepthPerspective'):
         self.robot_dynamics = False
         self.blocking = True
         self.time_step = 0.25
@@ -52,31 +52,30 @@ class VisualSim(Env):
         self.goal_position = np.array((10, 0, 0))
 
         # rewards
-        self.step_penalty = step_penalty
         self.collision_penalty = -1
         self.success_reward = 1
         self.max_time = 30
 
         # human
         self.human_num = 2
-        self.humans = defaultdict(list)
+        self.human_states = defaultdict(list)
         # 2.7 * 0.73
         self.human_radius = 1
 
         # robot
         self.max_speed = 1
         self.robot_radius = 0.3
-        self.robot = list()
+        self.robot_states = list()
 
         # action space
         self.speed_samples = 3
         self.rotation_samples = 7
-        self.actions = self.build_action_space()
+        self.actions = self._build_action_space()
         self.action_space = Discrete(self.speed_samples * self.rotation_samples + 1)
 
         # observation_space
         self.image_type = image_type
-        self.observation_space = Box(low=0, high=255, shape=(84, 84, ImageInfo[image_type].channel_size))
+        self.observation_space = Box(low=0, high=255, shape=(144, 256, ImageInfo[image_type].channel_size))
 
         # train test setting
         self.test_case_num = 5
@@ -93,15 +92,18 @@ class VisualSim(Env):
 
     def reset(self):
         self.time = 0
-        self.humans = defaultdict(list)
-        self.robot = list()
+        self.human_states = defaultdict(list)
+        self.robot_states = list()
 
         if self.robot_dynamics:
             self.client.reset()
         else:
             self.client.reset()
 
-        return self.compute_observation()
+        self._update_states()
+        obs = self.compute_observation()
+
+        return obs
 
     def step(self, action):
         import time
@@ -109,7 +111,7 @@ class VisualSim(Env):
         position = pose.position
         orientation = pose.orientation
         if self.robot_dynamics:
-            car_controls = self.interpret_action(action)
+            car_controls = self._interpret_action(action)
             self.client.setCarControls(car_controls)
             if self.blocking:
                 # pause for wall time self.time_step / self.clock_speed, which translates to game time self.time_step
@@ -117,19 +119,22 @@ class VisualSim(Env):
                 while not self.client.simIsPause():
                     time.sleep(0.001)
         else:
-            move = self.interpret_action(action)
-            if isinstance(move, ActionXY):
-                x = position.x_val + move.vx * self.time_step
-                y = position.y_val + move.vy * self.time_step
+            if isinstance(action, int):
+                action_index = action
+                action = self._interpret_action(action_index)
+
+            if isinstance(action, ActionXY):
+                x = position.x_val + action.vx * self.time_step
+                y = position.y_val + action.vy * self.time_step
                 yaw = np.arctan2(y, x)
-            elif isinstance(move, ActionRot):
+            elif isinstance(action, ActionRot):
                 _, _, yaw = airsim.to_eularian_angles(orientation)
-                yaw += move.r
-                x = position.x_val + move.v * np.cos(yaw)
-                y = position.y_val + move.v * np.sin(yaw)
+                yaw += action.r
+                x = position.x_val + action.v * np.cos(yaw) * self.time_step
+                y = position.y_val + action.v * np.sin(yaw) * self.time_step
             else:
                 raise NotImplementedError
-            self.move((x, y, self.initial_position[2]), yaw)
+            self._move((x, y, self.initial_position[2]), yaw)
             if self.blocking:
                 self.client.simContinueForTime(self.time_step / self.clock_speed)
                 # start = time.time()
@@ -137,11 +142,12 @@ class VisualSim(Env):
                     time.sleep(0.001)
                 # logging.debug('{:.2f}s past'.format(time.time() - start))
         self.time += self.time_step
+        self._update_states()
 
         pose = self.client.simGetVehiclePose()
         if not (np.isclose(pose.position.x_val, x) and np.isclose(pose.position.y_val, y)):
             logging.debug('Different pose values between simGetVehiclePose and simSetVehiclePose!!!')
-        reached_goal = self.distance_to_goal(pose.position) < self.robot_radius
+        reached_goal = self._distance_to_goal(pose.position) < self.robot_radius
         collision_info = self.client.simGetCollisionInfo()
 
         if reached_goal:
@@ -157,7 +163,7 @@ class VisualSim(Env):
             done = True
             info = 'Overtime'
         else:
-            reward = self.step_penalty
+            reward = 0
             done = False
             info = ''
 
@@ -168,7 +174,7 @@ class VisualSim(Env):
     def render(self, mode='human'):
         pass
 
-    def move(self, pos, yaw):
+    def _move(self, pos, yaw):
         self.client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(float(pos[0]), float(pos[1]), float(pos[2])),
                                                   airsim.to_quaternion(0, 0, yaw)), True)
 
@@ -182,7 +188,7 @@ class VisualSim(Env):
             img1d = np.array(response.image_data_float, dtype=np.float)
             img1d = 255 / np.maximum(np.ones(img1d.size), img1d)
             img2d = np.reshape(img1d, (response.height, response.width))
-            image = np.expand_dims(Image.fromarray(img2d).resize(self.observation_space.shape[:2]).convert('L'), axis=2)
+            image = np.expand_dims(Image.fromarray(img2d).convert('L'), axis=2)
         else:
             # get numpy array
             img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8)
@@ -191,7 +197,7 @@ class VisualSim(Env):
 
         # retrieve poses for both human and robot
         pose = self.client.simGetVehiclePose()
-        r = self.distance_to_goal(pose.position)
+        r = self._distance_to_goal(pose.position)
         phi = np.arctan2(self.goal_position[1] - pose.position.y_val, self.goal_position[0] - pose.position.x_val)
         goal = Goal(r, phi)
 
@@ -199,7 +205,7 @@ class VisualSim(Env):
 
         return observation
 
-    def compute_coordinate_observation(self, fov=True):
+    def _update_states(self):
         # retrieve all humans status
         for i in range(self.human_num):
             pose = self.client.simGetObjectPose('Human' + str(i))
@@ -212,72 +218,69 @@ class VisualSim(Env):
                 if trials >= 3:
                     logging.warning('Cannot get human status from client. Check human_num.')
                     break
-            self.humans[i].append(pose)
-        self.robot.append(self.client.simGetVehiclePose())
+            self.human_states[i].append(pose)
+        self.robot_states.append(self.client.simGetVehiclePose())
 
+    def compute_coordinate_observation(self, fov=True):
         # Todo: only consider humans in FOV
         human_states = []
         for i in range(self.human_num):
-            if len(self.humans[i]) == 1:
+            if len(self.human_states[i]) == 1:
                 vx = vy = 0
             else:
-                vx = (self.humans[i][-1].position.x_val - self.humans[i][-2].position.x_val) / self.time_step
-                vy = (self.humans[i][-1].position.y_val - self.humans[i][-2].position.y_val) / self.time_step
-            px = self.humans[i][-1].position.x_val
-            py = self.humans[i][-1].position.y_val
+                vx = (self.human_states[i][-1].position.x_val - self.human_states[i][-2].position.x_val) / self.time_step
+                vy = (self.human_states[i][-1].position.y_val - self.human_states[i][-2].position.y_val) / self.time_step
+            px = self.human_states[i][-1].position.x_val
+            py = self.human_states[i][-1].position.y_val
             human_state = ObservableState(px, py, vx, vy, self.human_radius)
             human_states.append(human_state)
 
-        px = self.robot[-1].position.x_val
-        py = self.robot[-1].position.y_val
-        if len(self.robot) == 1:
+        px = self.robot_states[-1].position.x_val
+        py = self.robot_states[-1].position.y_val
+        if len(self.robot_states) == 1:
             vx = vy = 0
         else:
             # TODO: use kinematics.linear_velocity?
-            vx = self.robot[-1].position.x_val - self.robot[-2].position.x_val
-            vy = self.robot[-1].position.y_val - self.robot[-2].position.y_val
+            vx = self.robot_states[-1].position.x_val - self.robot_states[-2].position.x_val
+            vy = self.robot_states[-1].position.y_val - self.robot_states[-2].position.y_val
         r  = self.robot_radius
         gx = self.goal_position[0]
         gy = self.goal_position[1]
         v_pref = 1
-        _, _, theta = airsim.to_eularian_angles(self.robot[-1].orientation)
+        _, _, theta = airsim.to_eularian_angles(self.robot_states[-1].orientation)
         robot_state = FullState(px, py, vx, vy, r, gx, gy, v_pref, theta)
 
         joint_state = JointState(robot_state, human_states)
 
         return joint_state
 
-    def interpret_action(self, action):
+    def _interpret_action(self, action_index):
+        assert isinstance(action_index, int)
         if self.robot_dynamics:
             car_controls = airsim.CarControls()
             car_controls.brake = 0
             car_controls.throttle = 1
-            if action == 0:
+            if action_index == 0:
                 car_controls.throttle = 0
                 car_controls.brake = 1
-            elif action == 1:
+            elif action_index == 1:
                 car_controls.steering = 0
-            elif action == 2:
+            elif action_index == 2:
                 car_controls.steering = 0.5
-            elif action == 3:
+            elif action_index == 3:
                 car_controls.steering = -0.5
-            elif action == 4:
+            elif action_index == 4:
                 car_controls.steering = 0.25
             else:
                 car_controls.steering = -0.25
             return car_controls
         else:
-            if isinstance(action, int):
-                return self.actions[action]
-            elif isinstance(action, ActionRot) or isinstance(action, ActionXY):
-                return action
-            else:
-                logging.error(action)
+            return self.actions[action_index]
 
-    def distance_to_goal(self, position):
+    def _distance_to_goal(self, position):
         return norm((self.goal_position[0] - position.x_val, self.goal_position[1] - position.y_val))
 
-    def build_action_space(self):
+    def _build_action_space(self):
         speeds = [(np.exp((i + 1) / self.speed_samples) - 1) / (np.e - 1) * self.max_speed for i in
                   range(self.speed_samples)]
         rotations = np.linspace(-np.pi / 4, np.pi / 4, self.rotation_samples)